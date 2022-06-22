5460412ed0592088e113406f75e9c9f6f41e4845
Jose Mena <andres.menaarias@gmail.com>
2022-06-21 22:15:50 -0500

    Ejemplos de omo parallel for y omp for

--- ejemplos/14_openmp/parallel_for/parallel_for.cpp
+++ ejemplos/14_openmp/parallel_for/parallel_for.cpp
@@ -5,28 +5,28 @@
 
 int main(int argc, char** args) {
     int threadsNum = omp_get_max_threads();
-    int threadsCreated = threadsNum;
-
-    if (argc == 2) {
+    if (argc >= 2) {
         threadsNum = atoi(args[1]);
+    }
+
+    int iterationsCount = threadsNum;
+
+    if (argc >= 3) {
+        iterationsCount = atoi(args[2]);
     }
 
     std::cout << "Hello from main thread!" << std::endl;
 
     double start_time = omp_get_wtime();
 
-    #pragma omp parallel num_threads(threadsNum) private(threadsCreated) \
-        shared(threadsNum, std::cout) default(none)
+    #pragma omp parallel for num_threads(threadsNum)
+    for (int i = 0; i < iterationsCount; ++i) {
+        #pragma omp critical
         {
-            threadsCreated = omp_get_num_threads();
-
-            #pragma omp critical
-            {
-                std::cout << omp_get_thread_num() << "/" << threadsCreated
-                    << ": Hello from secondary thread!" << std::endl;
-            }
+            std::cout << omp_get_thread_num() << "/" << omp_get_num_threads()
+                    << ": Iteration " << i << std::endl;
         }
-    
+    }
     double end_time = omp_get_wtime();
 
     std::cout << "Time elapsed: " << (end_time - start_time) << std::endl;
